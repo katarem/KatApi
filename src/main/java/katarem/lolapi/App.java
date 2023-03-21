1664e92cc0852671fc794a0fcb71bea313f2ceee
katarem <crismigesp@gmail.com>
2023-03-21 15:28:22 +0100

    item commit

--- src/main/java/katarem/lolapi/App.java
+++ src/main/java/katarem/lolapi/App.java
@@ -15,16 +15,8 @@ public class App
     {
         final String API_KEY = "RGAPI-cec537e5-5c13-4c01-8c12-57b2c7d285c7";
         final String lang = "es_ES";
-        SummonerService sService = new SummonerService(API_KEY, "euw1");
-
-        Summoner yo = sService.getSummoner("Tuernø");
-        MatchService mService = new MatchService(API_KEY, "europe");
-        ArrayList<String> gameIds = mService.getGames(yo.getPuuid(), API_KEY);
-        // System.out.println(gameIds.get(0));
-        ArrayList<Mastery> masteries = sService.getMateries(yo.getId());
         
         LoLService lService = new LoLService();
-        Map<String, Champion> campeones = lService.getChampionsData(lService.getLastVersion(), lang).getChampions();
         Map<String, Item> items;
         try {
             items = lService.getItemsData(API_KEY, lang).getData();
@@ -34,24 +26,8 @@ public class App
             e.printStackTrace();
         }
 
-        // for (Mastery maestria : masteries) {
-        //     System.out.println("Maestría " + maestria.getChampionLevel() + " " + getChamp(campeones.values(), maestria.getChampionId().toString()) + ": " + maestria.getChampionPoints() + " puntos");
-        // }
-
-       
-
-
 
     }
 
-    private static String getChamp(Collection<Champion> campeones, String id){
-        String result = "null";
-        for (Champion campeon : campeones) {
-            if(campeon.getKey().equals(id)){
-                result = campeon.getName();
-                break;
-            }
-        }
-        return result;
-    }
+    
 }
